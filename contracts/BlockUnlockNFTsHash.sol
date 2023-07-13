617460ab247945c62317312674407f650509433e
Dilum Bandara <33807852+dilumb@users.noreply.github.com>
2023-07-13 19:51:32 +1000

    Clean up BlockUnlockNFTsHash.sol

--- contracts/BlockUnlockNFTsHash.sol
+++ contracts/BlockUnlockNFTsHash.sol
@@ -7,7 +7,6 @@ pragma solidity ^0.8.0;
 
 // Import standard NFT contracts from OpenZeppelin
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract BlockUnlockNFTsHash is ERC721URIStorage {
     /**
